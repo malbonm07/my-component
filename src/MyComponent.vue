2972ba31cdcf1b5851790b39a48c36eb1a794283
Miguel Alonso <malbonm07@gmail.com>
2019-08-18 20:55:19 -0500

    changes

---
+++ src/MyComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button class="btn" :class="color">
+            {{name}}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        color: {
+            type: String,
+            required: true
+        },
+    }
+}
+</script>
+
+<style>
+.btn {
+    width: 150px;
+    height: 50px;
+    outline: none;
+}
+</style>
+
